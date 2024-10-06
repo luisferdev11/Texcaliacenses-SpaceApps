9ab19273e82ccdd5b245ecd25b379c440823871a
danielctecla <daniel.ctecla@gmail.com>
2024-10-06 01:14:43 -0600

    [pre-chat] add: pre-chat

--- Frontend/texcaliacenses-spaceapps/src/main.jsx
+++ Frontend/texcaliacenses-spaceapps/src/main.jsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 
 import Index from "./pages/index.jsx";
 import Report from "./pages/report.jsx";
+import Chat from "./pages/chat.jsx";
 
 import "./index.css";
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")).render(
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/report/:city/:state/:country" element={<Report />} />
+      <Route path="/chat" element={<Chat />} />
     </Routes>
   </Router>
 );
--- Frontend/texcaliacenses-spaceapps/src/pages/report.jsx
+++ Frontend/texcaliacenses-spaceapps/src/pages/report.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Header from "../components/common/header.jsx";
-import Footer from "../components/common/footer.jsx";
+import HorizontalSpliter from "../components/common/spliter.jsx";
 import Summary from "../components/reports/summary.jsx";
 import Actions from "../components/reports/actions.jsx";
 
 import MicIcon from "../components/icons/mic.jsx";
 
 export default function Report() {
+  const [showChat, setShowChat] = useState(false);
   const { city, state, country } = useParams();
 
   const action_list = [
@@ -30,28 +31,62 @@ export default function Report() {
     },
   ];
 
+  const handleChat = () => {
+    setShowChat(!showChat);
+  };
+
   return (
     <>
       <div className="h-[100dvh] w-[100dvw] flex flex-col">
+        {/* Header */}
         <Header
           text={`Reporte para ${state}, ${country}`}
           className="mx-auto md:max-w-4xl"
         />
         <div className="h-[1.5px] bg-neutral-300 mb-3" />
 
-        <main className="flex-grow w-full mx-auto md:max-w-4xl px-2 space-y-3 overflow-y-auto">
-          <Summary temperature={25} precipitation={60} humidity={80} />
-          <Actions action_list={action_list} />
-        </main>
+        {/* --------------------Container------------------- */}
+        <main
+          className={`flex-grow w-full mx-auto ${
+            showChat ? "md:max-w-7xl" : "md:max-w-4xl"
+          }`}
+        >
+          <HorizontalSpliter
+            initialSize={showChat ? 50 : 0}
+            style={{ transition: "width 0.9s" }}
+          >
+            {/* ------------------------Chat logic AI------------------------------- */}
+            <div
+              className={`${showChat ? "block" : "hidden"} h-full w-full px-2`}
+            >
+              <section className="h-full w-full bg-black"></section>
+            </div>
 
-        <footer className="w-full mx-auto md:max-w-4xl px-2 pb-10 pt-4 md:pt-10 md:pb-24">
-          <section className="w-full max-w-xs sm:max-w-sm md:max-w-lg mx-auto flex flex-col gap-3">
-            <button className="w-full py-2 bg-black rounded-md text-white inline-flex items-center justify-center gap-2">
-              <MicIcon className="size-3.5" />
-              Preguntar al Asistente
-            </button>
-          </section>
-        </footer>
+            <div className="flex flex-grow h-full flex-col">
+              {/* ----------------------------Summary------------------- */}
+              <section className="flex-grow w-full px-2 space-y-3 overflow-y-auto">
+                <Summary temperature={25} precipitation={60} humidity={80} />
+                <Actions action_list={action_list} />
+              </section>
+
+              <div
+                className={`w-full mx-auto md:max-w-4xl px-2 pb-10 pt-4 md:pt-10 md:pb-24
+                ${showChat ? "hidden" : "block"}
+                `}
+              >
+                <section className="w-full max-w-xs sm:max-w-sm md:max-w-lg mx-auto flex flex-col gap-3">
+                  <button
+                    className="w-full py-2 bg-black rounded-md text-white inline-flex items-center justify-center gap-2"
+                    onClick={handleChat}
+                  >
+                    <MicIcon className="size-3.5" />
+                    Preguntar al Asistente
+                  </button>
+                </section>
+              </div>
+            </div>
+          </HorizontalSpliter>
+        </main>
       </div>
     </>
   );
